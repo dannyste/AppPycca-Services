b14e24f98184f211849a35c319c1cb86d63ff24c
Danny Ponce <dponceld@gmail.com>
2018-12-02 23:47:39 -0500

    modificaciones.

--- api/application/controllers/Client.php
+++ api/application/controllers/Client.php
@@ -13,6 +13,19 @@ class Client extends REST_Controller {
       $this->load->helper('util');
   }
 
+  public function validate_client_get() {
+    $document_type = $this->uri->segment(3);
+    $document_number = $this->uri->segment(4);
+    $club_pycca_card_number = $this->uri->segment(5);
+    if (!isset($document_type) OR !isset($document_number) OR !isset($club_pycca_card_number)) {
+      $response = response_format(FALSE, 'Número de parámetros incorrectos.');
+      $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+      return;
+    }
+    $response = $this->Client_model->validate_client($document_type, $document_number, $club_pycca_card_number);
+    $this->response($response);
+  }
+
   public function account_status_get() {
     $club_pycca_card_number = $this->uri->segment(3);
     if (!isset($club_pycca_card_number)) {
@@ -24,17 +37,22 @@ class Client extends REST_Controller {
     $this->response($response);
   }
 
-  public function card_blocking_post() {
-    $club_pycca_card_number = $this->post('club_pycca_card_number');
+  public function quota_increase_post() {
+    $increase_type = $this->post('increase_type');
     $account_number = $this->post('account_number');
-    $reason_code = $this->post('reason_code');
-    $reason_description = $this->post('reason_description');
-    if (!isset($club_pycca_card_number) OR !isset($account_number) OR !isset($reason_code) OR !isset($reason_description)) {
+    $identification = $this->post('identification');
+    $email = $this->post('email');
+    $name = $this->post('name');
+    $last_name = $this->post('last_name');
+    $quota = $this->post('quota');
+    if (!isset($increase_type) OR !isset($account_number) OR !isset($identification)
+        OR !isset($email) OR !isset($name) OR !isset($last_name)
+        OR !isset($quota)) {
       $response = response_format(FALSE, 'Número de parámetros incorrectos.');
       $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
       return;
     }
-    $response = $this->Client_model->card_blocking($club_pycca_card_number, $account_number, $reason_code, $reason_description);
+    $response = $this->Client_model->quota_increase($increase_type, $account_number, $identification, $email, $name, $last_name, $quota);
     $this->response($response);
   }
 
@@ -49,16 +67,29 @@ class Client extends REST_Controller {
     $this->response($response);
   }
 
-  public function validate_client_get() {
-    $document_type = $this->uri->segment(3);
-    $document_number = $this->uri->segment(4);
-    $club_pycca_card_number = $this->uri->segment(5);
-    if (!isset($document_type) OR !isset($document_number) OR !isset($club_pycca_card_number)) {
+  public function cards_get() {
+    $account_number = $this->uri->segment(3);
+    $club_pycca_card_number = $this->uri->segment(4);
+    if (!isset($account_number) OR !isset($club_pycca_card_number)) {
       $response = response_format(FALSE, 'Número de parámetros incorrectos.');
       $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
       return;
     }
-    $response = $this->Client_model->validate_client($document_type, $document_number, $club_pycca_card_number);
+    $response = $this->Client_model->cards($account_number, $club_pycca_card_number);
+    $this->response($response);
+  }
+
+  public function card_blocking_post() {
+    $club_pycca_card_number = $this->post('club_pycca_card_number');
+    $account_number = $this->post('account_number');
+    $reason_code = $this->post('reason_code');
+    $reason_description = $this->post('reason_description');
+    if (!isset($club_pycca_card_number) OR !isset($account_number) OR !isset($reason_code) OR !isset($reason_description)) {
+      $response = response_format(FALSE, 'Número de parámetros incorrectos.');
+      $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+      return;
+    }
+    $response = $this->Client_model->card_blocking($club_pycca_card_number, $account_number, $reason_code, $reason_description);
     $this->response($response);
   }
 
@@ -80,37 +111,6 @@ class Client extends REST_Controller {
     }
     $response = $this->Client_model->club_pycca_partner($name, $last_name, $born_date, $identification,
                                                         $email, $phone, $cell_phone, $address);
-    $this->response($response);
-  }
-
-  public function club_pycca_cards_get() {
-    $account_number = $this->uri->segment(3);
-    $club_pycca_card_number = $this->uri->segment(4);
-    if (!isset($account_number) OR !isset($club_pycca_card_number)) {
-      $response = response_format(FALSE, 'Número de parámetros incorrectos.');
-      $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
-      return;
-    }
-    $response = $this->Client_model->club_pycca_cards($account_number, $club_pycca_card_number);
-    $this->response($response);
-  }
-
-  public function quota_increase_post() {
-    $increase_type = $this->post('increase_type');
-    $account_number = $this->post('account_number');
-    $identification = $this->post('identification');
-    $email = $this->post('email');
-    $name = $this->post('name');
-    $last_name = $this->post('last_name');
-    $quota = $this->post('quota');
-    if (!isset($increase_type) OR !isset($account_number) OR !isset($identification)
-        OR !isset($email) OR !isset($name) OR !isset($last_name)
-        OR !isset($quota)) {
-      $response = response_format(FALSE, 'Número de parámetros incorrectos.');
-      $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
-      return;
-    }
-    $response = $this->Client_model->quota_increase($increase_type, $account_number, $identification, $email, $name, $last_name, $quota);
     $this->response($response);
   }
 
--- api/application/models/Client_model.php
+++ api/application/models/Client_model.php
@@ -10,6 +10,43 @@ class Client_model extends CI_Model {
       $this->load->helper('util');
   }
 
+  public function validate_client($document_type, $document_number, $club_pycca_card_number) {
+    try {
+      $sql = "EXEC IVDBINVENTAR.vtex._validaClienteConsultas '$document_type', '$document_number', '$club_pycca_card_number'";
+      $query = $this->db->query($sql);
+      $error_db = $this->db->error();
+      if ((int)$error_db['code'] <> 0) {
+        throw new Exception($error_db['message'], $error_db['code']);
+      }
+      $data = array();
+      $resultset = 0;
+      do {
+        $row = $query->row_array();
+        if ($resultset == 0) {
+          $data['status_error'] = array(
+            'co_error' => (int)$row['co_error'],
+            'tx_error' => $row['tx_error']
+          );
+        }
+        else {
+          $data['result'] = array(
+            'ma_cuenta'    => (int)$row['ma_cuenta'],
+            'cl_nombres'   => utf8_encode($row['cl_nombres']),
+            'cl_apellidos' => utf8_encode($row['cl_apellidos']),
+            'no_estados'   => $row['no_estados'],
+            'ma_fapertura' => $row['ma_fapertura']
+          );
+        }
+        $resultset++;
+      } while ($query->_next_resultset());
+      $query->free_result();
+      return response_format(TRUE, 'Servicio ejecutado correctamente.', $data);
+    }
+    catch (Exception $e) {
+      return response_format(FALSE, $e->getMessage());
+    }
+  }
+
   public function account_status($club_pycca_card_number) {
     try {
       $sql = "EXEC NTS_TARJCRED.dbo.sp_webtcp_ws_APP '$club_pycca_card_number'";
@@ -47,11 +84,9 @@ class Client_model extends CI_Model {
     }
   }
 
-  public function card_blocking($club_pycca_card_number, $account_number, $reason_code, $reason_description) {
+  public function quota_increase($increase_type, $account_number, $identification, $email, $name, $last_name, $quota) {
     try {
-      $sql = "DECLARE @p7 VARCHAR(8000)
-              EXEC NTS_TARJCRED.dbo.sp_tactestadostarj 'sa', 'PYCCA_APP', 0, 1000, 0, 27, @p7 OUTPUT, 't119', '$club_pycca_card_number', $account_number, '0000000000', ' ', '1', $reason_code, NULL, 'N', '$reason_description', '840', NULL
-              SELECT @p7 AS result";
+      $sql = "EXEC NTS_TARJCRED.dbo._enviar_solicitud_aumento_cupo '$increase_type', '$account_number', '$identification', '$email', '$name', '$last_name', '$quota'";
       $query = $this->db->query($sql);
       $error_db = $this->db->error();
       if ((int)$error_db['code'] <> 0) {
@@ -60,8 +95,9 @@ class Client_model extends CI_Model {
       $data = array();
       $row = $query->row_array();
       $data['status_error'] = array(
-        'co_error' => '',
-        'tx_error' => '');
+        'co_error' => (int)$row['co_error'],
+        'tx_error' => utf8_encode($row['tx_error'])
+      );
       $query->free_result();
       return response_format(TRUE, 'Servicio ejecutado correctamente.', $data);
     }
@@ -107,9 +143,9 @@ class Client_model extends CI_Model {
     }
   }
 
-  public function validate_client($document_type, $document_number, $club_pycca_card_number) {
+  public function cards($account_number, $club_pycca_card_number) {
     try {
-      $sql = "EXEC IVDBINVENTAR.vtex._validaClienteConsultas '$document_type', '$document_number', '$club_pycca_card_number'";
+      $sql = "EXEC NTS_TARJCRED.dbo.sp_ttarjetasactivas '$account_number', '$club_pycca_card_number'";
       $query = $this->db->query($sql);
       $error_db = $this->db->error();
       if ((int)$error_db['code'] <> 0) {
@@ -118,24 +154,46 @@ class Client_model extends CI_Model {
       $data = array();
       $resultset = 0;
       do {
-        $row = $query->row_array();
         if ($resultset == 0) {
+          $row = $query->row_array();
           $data['status_error'] = array(
             'co_error' => (int)$row['co_error'],
             'tx_error' => $row['tx_error']
           );
         }
         else {
-          $data['result'] = array(
-            'ma_cuenta'    => (int)$row['ma_cuenta'],
-            'cl_nombres'   => utf8_encode($row['cl_nombres']),
-            'cl_apellidos' => utf8_encode($row['cl_apellidos']),
-            'no_estados'   => $row['no_estados'],
-            'ma_fapertura' => $row['ma_fapertura']
-          );
+          foreach($query->result_array() as $row) {
+            $data['result'][] = array(
+              'tarjeta'         => $row['tarjeta'],
+              'ta_plnombre1'    => utf8_encode($row['ta_plnombre1']),
+              'ta_princiadicio' => $row['ta_princiadicio']
+            );
+          }
         }
         $resultset++;
       } while ($query->_next_resultset());
+      $query->free_result();
+      return response_format(TRUE, 'Servicio ejecutado correctamente.', $data);
+    }
+    catch (Exception $e) {
+      return response_format(FALSE, $e->getMessage());
+    }
+  }
+
+  public function card_blocking($club_pycca_card_number, $account_number, $reason_code, $reason_description) {
+    try {
+      $sql = "EXEC NTS_TARJCRED.._bloquea_tarjeta '$club_pycca_card_number', $account_number, '$reason_code', '$reason_description'";
+      $query = $this->db->query($sql);
+      $error_db = $this->db->error();
+      if ((int)$error_db['code'] <> 0) {
+        throw new Exception($error_db['message'], $error_db['code']);
+      }
+      $data = array();
+      $row = $query->row_array();
+      $data['status_error'] = array(
+        'co_error' => (int)$row['co_error'],
+        'tx_error' => utf8_encode($row['tx_error'])
+      );
       $query->free_result();
       return response_format(TRUE, 'Servicio ejecutado correctamente.', $data);
     }
@@ -167,60 +225,4 @@ class Client_model extends CI_Model {
     }
   }
 
-  public function club_pycca_cards($account_number, $club_pycca_card_number) {
-    try {
-      $sql = "EXEC NTS_TARJCRED.dbo.sp_ttarjetasactivas '$account_number', '$club_pycca_card_number'";
-      $query = $this->db->query($sql);
-      $error_db = $this->db->error();
-      if ((int)$error_db['code'] <> 0) {
-        throw new Exception($error_db['message'], $error_db['code']);
-      }
-      $data = array();
-      $resultset = 0;
-      do {
-        $row = $query->row_array();
-        if ($resultset == 0) {
-          $data['status_error'] = array(
-            'co_error' => (int)$row['co_error'],
-            'tx_error' => $row['tx_error']
-          );
-        }
-        else {
-          $data['result'] = array(
-            'tarjeta'    => (int)$row['tarjeta'],
-            'ta_plnombre1'   => utf8_encode($row['ta_plnombre1']),
-            'ta_princiadicio' => $row['ta_princiadicio']
-          );
-        }
-        $resultset++;
-      } while ($query->_next_resultset());
-      $query->free_result();
-      return response_format(TRUE, 'Servicio ejecutado correctamente.', $data);
-    }
-    catch (Exception $e) {
-      return response_format(FALSE, $e->getMessage());
-    }
-  }
-
-  public function quota_increase($increase_type, $account_number, $identification, $email, $name, $last_name, $quota) {
-    try {
-      $sql = "EXEC NTS_TARJCRED.dbo._enviar_solicitud_aumento_cupo '$increase_type', '$account_number', '$identification', '$email', '$name', '$last_name', '$quota'";
-      $query = $this->db->query($sql);
-      $error_db = $this->db->error();
-      if ((int)$error_db['code'] <> 0) {
-        throw new Exception($error_db['message'], $error_db['code']);
-      }
-      $data = array();
-      $row = $query->row_array();
-      $data['status_error'] = array(
-        'co_error' => (int)$row['co_error'],
-        'tx_error' => utf8_encode($row['tx_error'])
-      );
-      $query->free_result();
-      return response_format(TRUE, 'Servicio ejecutado correctamente.', $data);
-    }
-    catch (Exception $e) {
-      return response_format(FALSE, $e->getMessage());
-    }
-  }
 }
